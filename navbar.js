d074c183ab6e612601f7cecc3765288a77b01a36
atultyagi612 <69669017+atultyagi612@users.noreply.github.com>
2021-01-19 13:25:08 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,20 @@
+let naavside = () => {
+    let sideview = document.querySelector('.sideview');
+    let nav = document.querySelector('.navlinks');
+    let navlinks = document.querySelectorAll('.navlinks li')
+    sideview.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navlinkslide 1s ease forwards ${index / 7}s`;
+            }
+
+        });
+        sideview.classList.toggle('toggle');
+    });
+
+}
+naavside();
