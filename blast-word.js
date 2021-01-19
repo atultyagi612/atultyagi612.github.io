0c50bdb61c58497baea47b528944c8929620bfa0
atultyagi612 <69669017+atultyagi612@users.noreply.github.com>
2021-01-19 14:58:33 +0530

    Add files via upload

---
+++ blast-word.js
@@ -0,0 +1,25 @@
+let elemnt = () => {
+         
+    let element = document.getElementsByClassName('rubberBand')
+
+    for (let i = 0; i < element.length; i++) {
+        element[i].style.animation = `rubberBand 1s, always1 5s infinite 1s, color-trans 30s infinite 1s`;
+        element[i].addEventListener('mouseover', () => {
+            if (element[i].style.animation) {
+                element[i].style.animation = '';
+            }
+            else {
+                element[i].style.animation = `rubberBand 1s, always1 5s infinite 1s, color-trans 30s infinite 1s`;
+            }
+
+        }, false);
+        element[i].addEventListener('mouseout', () => {
+                element[i].style.animation = `rubberBand 1s, always1 5s infinite 1s, color-trans 30s infinite 1s`;
+        }, false);
+        element[i].addEventListener('click', () => {
+                element[i].style.animation = `rubberBand 1s, always1 5s infinite 1s, color-trans 30s infinite 1s`;
+        }, false);
+
+    }
+}
+elemnt();
