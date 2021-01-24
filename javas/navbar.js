d010c511c88cca4cb9b3454be7c3c324d77bce45
atultyagi612 <69669017+atultyagi612@users.noreply.github.com>
2021-01-24 17:32:44 +0530

    Add files via upload

---
+++ javas/navbar.js
@@ -0,0 +1,39 @@
+let naavside = () => {
+    let sideview = document.querySelector('.sideview');
+    let nav = document.querySelector('.navlinks');
+    let navlinks = document.querySelectorAll('.navlinks li')
+    sideview.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navlinkslide 1s ease forwards ${index / 7}s`;
+            }
+
+        });
+        sideview.classList.toggle('toggle');
+    });
+    nav.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navlinkslide 1s ease forwards ${index / 7}s`;
+            }
+
+        });
+        sideview.classList.toggle('toggle');
+    });
+    
+}
+naavside();
+
+
+window.addEventListener('scroll',function () {
+    let header =document.querySelector('nav');
+    header.classList.toggle('scroll-active',window.scrollY > 0);
+})
