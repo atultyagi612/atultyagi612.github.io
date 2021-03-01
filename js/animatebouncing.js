cc92b9453d3e5b10e284ff54446737bf812e2e01
atultyagi612 <69669017+atultyagi612@users.noreply.github.com>
2021-03-01 17:00:03 +0530

    Add files via upload

--- js/animatebouncing.js
+++ js/animatebouncing.js
@@ -3,21 +3,21 @@ let elemnt = () => {
     let element = document.getElementsByClassName('rubberBand')
 
     for (let i = 0; i < element.length; i++) {
-        element[i].style.animation = `rubberBand 1s, color-trans 30s infinite ${i/6}s`;
+        element[i].style.animation = `rubberBand 1s, color-trans 20s infinite ${i/9}s`;
         element[i].addEventListener('mouseover', () => {
             if (element[i].style.animation) {
                 element[i].style.animation = '';
             }
             else {
-                element[i].style.animation = `rubberBand 1s,  color-trans 30s infinite 1s`;
+                element[i].style.animation = `rubberBand 1s,  color-trans 20s infinite ${i/9}s`;
             }
 
         }, false);
         element[i].addEventListener('mouseout', () => {
-                element[i].style.animation = `rubberBand 1s, color-trans 30s infinite 1s`;
+                element[i].style.animation = `rubberBand 1s, color-trans 20s infinite ${i/9}s`;
         }, false);
         element[i].addEventListener('click', () => {
-                element[i].style.animation = `rubberBand 1s,  color-trans 30s infinite 1s`;
+                element[i].style.animation = `rubberBand 1s,  color-trans 20s infinite ${i/9}s`;
         }, false);
 
     }
